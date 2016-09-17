901ddf279eaf4381de871862c15c92e9f18dbeb4
brilacerda <gabrielamota29@gmail.com>
2016-09-17 12:56:04 -0300

    Algoritmo de verificação de solução

--- MiniProjeto/binary8queens.py
+++ MiniProjeto/binary8queens.py
@@ -17,8 +17,59 @@ population = {}
 sample = ['000', '001', '010', '011', '100', '101', '110', '111']
 print (population)
 
-def checkIndividual(ind):
-	return ind
+def hasDuplicatedColumn(ind):
+		'''
+			Transforms the individual into a set and see if there is any 
+			repeated numbers, indicating the set to be smaller then 8.
+		'''
+	 if len(set(ind)) < 8:
+	 	return True
+	 else: 
+	 	return False
+
+def hasMatchAtBottomToTopDiagonal(ind):
+		'''
+			Identify all diagonals from left to right, from the bottom to 
+			the top by the sum of it. In the same diagonal the sum must 
+			be the equal.
+		'''
+	indSum = []
+	for i in range(8):
+		indSum.append(i + ind[i])
+	
+	if len(set(indSum)) < 8:
+		return True
+	else: 
+	 	return False
+
+def hasMatchAtToptoBottomDiagonal(ind):
+		'''
+			Identify all diagonals from left to right from the top to the 
+			bottom by the reduction of the coordinates to the extremes -
+			left and top - and comparing it.
+		'''
+	diag = []
+	for i in range(8):
+		# linha < coluna
+		if ind[i] < i:
+			diag.append((0, i - ind[i]))
+		else:
+			diag.append((ind[i] - i, 0))
+	print (diag)
+	if len(set(diag)) < 8:
+		return True
+	else: 
+	 	return False		
+
+def checkSolution(ind):
+	if hasDuplicatedColumn(ind):
+		return True
+	elif hasMatchAtBottomToTopDiagonal(ind):
+		return True
+	elif hasMatchAtToptoBottomDiagonal(ind):
+		return True
+	else:
+		return False
 
 # def individualToBinary(ind):
 # 	temp = []
@@ -35,8 +86,8 @@ for i in range(10):
 	shuffle(sample)
 
 	# Avaliação da solução
-	# if checkIndividual(sample)
-	# 	print ("Solution found at %s" % i, temp)
+	# if checkSolution(sample)
+	# 	print ("Solution %s found at iteration #%d" % sample, i)
 	# 	break
 
 	population[i] = sample;
