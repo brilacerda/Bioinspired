e7b711cb18f456321d03676a609092443b03cfee
brilacerda <gabrielamota29@gmail.com>
2016-10-10 20:11:14 -0300

    Generate individuals and a small structure

--- MiniProjeto2/ackleyEvolutionaryStrategy.py
+++ MiniProjeto2/ackleyEvolutionaryStrategy.py
@@ -1,13 +1,53 @@
 from math import pi, cos, sqrt, exp
+from random import random
 
 a = 20
 b = 0.2
 c = 2*pi
 n = 6
 x = 3
-individual = [1, 0, 3, 2, 4, 2]
+population_size = 50
+convergedAt = []
+population = []
+minimum = 100
+# Qual será o tamanho da população?
 
-def calculateAckley():
+def main():
+	minimum = 100
+	# Inicialização aleatória
+	for i in range(population_size):
+		sample = generateIndividual()
+
+		# Avaliação da solução
+		fx = calculateAckley(sample)
+		if fx < minimum:
+			minimum = fx
+			print ("f(x) = ", fx)
+		if not fx:
+			population.append(sample)
+			print ("f(x)=0 when ", sample)
+			convergedAt.append(i)
+		else:
+			population.append(sample)
+			#print ("ind #%s "%i, sample)
+
+
+def generateIndividual():
+	individual = []
+	for i in range(n):
+		v = random()*10
+		if v <= 30:
+			v-=15
+		elif v <= 60:
+			v-=45
+		elif v <= 90:
+			v-=75
+		else:
+			v = ((v-90)*3)-15
+		individual.append(v)
+	return individual
+
+def calculateAckley(individual):
 	sum1 = 0
 	sum2 = 0
 
