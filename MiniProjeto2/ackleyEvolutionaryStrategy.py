07a203a206c666f1f38b797ccd11b29d2355ed3c
brilacerda <gabrielamota29@gmail.com>
2016-10-15 16:39:52 -0300

    Comments & minimum fix

--- MiniProjeto2/ackleyEvolutionaryStrategy.py
+++ MiniProjeto2/ackleyEvolutionaryStrategy.py
@@ -1,4 +1,4 @@
-from math import pi, cos, sqrt, exp
+from math import pi, cos, sqrt, exp, fabs
 from random import random, randrange, sample
 
 a = 20
@@ -11,22 +11,20 @@ population = []
 kids = []
 minimum = 1e20
 
-# 1. Qual será o tamanho da população?
-# 2. Gostaria de inserir uma parcela dos 
-# indivíduos com zeros em sua composição.
+
 # 3. Usar também a recombinação feita por mhss no
 # Mini-Projeto 1
 
 class Candidate:
 	def __init__(self, value=None):
 		if value is None:
-			# inicial aleatório
+			# Aleatory initialization
 			self.value = [random()*30.0 - 15.0 for _ in range(n)]
 		else:
-			# filho de dois pais
+			# 2 parents child
 			self.value = value
 
-		# ao criar o candidato, já calcula o fitness
+		# fitness is always calculated, even when the candidate is created 
 		self.calc_fitness()
 
 	def calc_fitness(self):
@@ -38,13 +36,14 @@ class Candidate:
 		self.fitness = -a*exp(-b*sqrt(sum1/n)) - exp(sum2/n) + a + 1
 
 		global minimum
-		if self.fitness < minimum:
+		# Absolute value of the fitness
+		if fabs(self.fitness) < minimum:
 			minimum = self.fitness
 
 	def cross(self, other, cut):
 		new_value = self.value[:cut] + other.value[cut:]
 
-		# mutação
+		# mutation
 		if random() < 0.1:
 			new_value[randrange(n)] = random()*30.0 - 15.0
 
@@ -54,19 +53,23 @@ def main():
 	global minimum
 	minimum = 1e20
 
-	# Inicialização aleatória
+	# Aleatory initialization
 	global population
-	population = [Candidate() for i in range(population_size)]
 
+	# Aleatory sample
+	population = [Candidate() for i in range(population_size)]
+	# 200 generations
 	for z in range(200):
+		# 100 kids generated
 		for y in range(100):
 			recombination()
 		survivorsSelection()
-		print(minimum)
+		print(z, "generation", minimum)
 
 def recombination():
 	'''
-		intermediary
+		2 aleatory parents and an aleatory cut
+		The best out of the 2 children
 	'''
 	inds = sample(population, 2)
 	cut = randrange(1, n)
@@ -80,6 +83,9 @@ def recombination():
 		kids.append(child2)
 
 def survivorsSelection():
+	'''
+		200 bests between the population and children 
+	'''
 	global population
 	global kids
 
@@ -90,4 +96,3 @@ def survivorsSelection():
 
 if __name__ == "__main__":
 	main()
-
