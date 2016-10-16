b126c1e6738faffbe1ca4b9a85a1e9a3e12ce459
mhss <mhss@cin.ufpe.br>
2016-10-16 18:30:11 -0300

    version with big population

--- MiniProjeto2/ackleyEvolutionaryStrategy.py
+++ MiniProjeto2/ackleyEvolutionaryStrategy.py
@@ -1,14 +1,16 @@
 import math
 from math import cos, sqrt, exp
-from random import random, randrange, sample, gauss, seed
+from random import random, randrange, sample, gauss, seed, shuffle
 
-# testa mudar esses parametros
 n = 30
+min_v, max_v = -15.0, 15.0
+# testa mudar esses parametros
 t1 = 3.0# 1.0/sqrt(2*n)
 t2 = 3.0# 1.0/sqrt(2*sqrt(n))
-min_step, max_step = 1e-5, 10
-min_v, max_v = -15.0, 15.0
-iterations = 100000
+min_step, max_step = 1e-8, 10
+population_size = 20
+children_ratio = 7
+generations = 500
 
 minimum_fitness = 1e20
 
@@ -33,7 +35,7 @@ class Candidate:
 			# child of somebody
 			self.value, self.step = value, step
 
-		# fitness is always calculated, even when the candidate is created 
+		# fitness is always calculated, even when the candidate is created
 		self.calc_fitness()
 
 	def calc_fitness(self):
@@ -49,22 +51,52 @@ class Candidate:
 		if self.fitness < minimum_fitness:
 			minimum_fitness = self.fitness
 
-	def cross_alone(self):
+	def walk(self):
 		global_step = t1*gauss(0, 1)
-		new_step = [disturb_step(x, global_step) for x in self.step]
-		new_value = [disturb(self.value[i], new_step[i]) for i in range(n)]
-		return Candidate(new_value, new_step)
+		self.step = [disturb_step(x, global_step) for x in self.step]
+		self.value = [disturb(self.value[i], self.step[i]) for i in range(n)]
 
+		# recalculate fitness
+		self.calc_fitness()
+
+	# tenta mudar esse operador se quiser
 	def cross(self, other):
 		new_v, new_s = [], []
 		for i in range(n):
-			new_v.append(rand_in_range(self.value[i], other.value[i]))
+			if randrange(2) == 0:
+				new_v.append(self.value[i])
+			else:
+				new_v.append(other.value[i])
 			new_s.append(rand_in_range(self.step[i], other.step[i]))
 
 		return Candidate(new_v, new_s)
 
-# mi+lambda
-population_size = 20
+def new_generation(population):
+	kids = []
+	for _ in range(children_ratio*population_size):
+		parents = sample(population, 2)
+		child = parents[0].cross(parents[1])
+		kids.append(child)
+	return kids
+
+def mario_breeding(pop):
+	kids = []
+	tot = len(pop)
+	if tot <= 1:
+		return kids
+
+	q1, q2 = tot>>1, (tot+1)>>1
+	half1, half2 = pop[q2:], pop[:q2]
+
+	for _ in range(children_ratio):
+		shuffle(half1)
+		kids += [half1[i].cross(half2[i]) for i in range(q1)]
+
+	return kids + mario_breeding(half2)
+
+def selectSurvivors(population):
+	population.sort(key=lambda x: x.fitness)
+	return population[:population_size]
 
 def run_big_population():
 	seed()
@@ -72,41 +104,27 @@ def run_big_population():
 
 	for _ in range(10):
 		minimum_fitness = 1e20
-
 		# Aleatory initialization
 		population = [Candidate() for i in range(population_size)]
 
-		for i in range(10000):
-			kids = new_generation(population)
+		# for every generation
+		for i in range(generations):
+			kids = mario_breeding(sorted(population, key=lambda x: x.fitness))
+			# kids = new_generation(population)
+
+			# updates every candidate with its ES parameters
 			for x in population:
 				x.walk()
 			for x in kids:
 				x.walk()
 
-			population = selectSurvivors()
+			population = selectSurvivors(population + kids)
+			# population = selectSurvivors(kids)
+			if i%100 == 0:
+				print(population[0].fitness)
 
 		print(minimum_fitness)
 
-def new_generation(population):
-	kids = []
-	for _ in range(700):
-		parents = sample(population, 2)
-		child = parents[0].cross(parents[1])
-		kids.append(child)
-	return kids
-
-def survivorsSelection():
-	'''
-		200 bests between the population and children 
-	'''
-	global population
-	global kids
-
-	wholeCrew = population + kids
-	wholeCrew.sort(key=lambda x: x.fitness)
-	population = wholeCrew[:population_size]
-	kids = []
-
 if __name__ == "__main__":
-	run_single_individual()
+	run_big_population()
 
